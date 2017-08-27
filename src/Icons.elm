49e323ae591dbe8c87d2155733f3fdd878572bc6
Rafael Cintra <rafael@apnic.net>
2017-08-28 09:08:30 +1000

    Code cleanup.

--- src/Icons.elm
+++ src/Icons.elm
@@ -81,8 +81,8 @@ zoomOutIcon =
                    SvgA.d "M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z M 3 4 h 10"] [Svg.title [] [text "Zoom out"]],
             Svg.rect [SvgA.x "6", SvgA.y "8.5", SvgA.width "7", SvgA.height "2"][Svg.title [] [text "Zoom out"]]]
 
-zoomOutIcon2 : Html a
-zoomOutIcon2 =
+zoomOutCircleIcon : Html a
+zoomOutCircleIcon =
     Svg.svg [SvgA.viewBox "0 0 24 24", SvgA.class "zoomOutIcon"]
         [
            Svg.defs [] [Svg.mask [SvgA.id "zoomOutMask"] [
